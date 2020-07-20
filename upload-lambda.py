b9c881775eb9a9fd336ce545123d01b03ba7666e
Jack Kinsey <jack@hexbitstudios.com>
2020-07-20 01:25:15 -0400

    feat: modify code after testing in Lambda

--- upload-lambda.py
+++ upload-lambda.py
@@ -1,25 +1,33 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-build_bucket = s3.Bucket('codebuild.jackwkinsey.com')
-portfolio_bucket = s3.Bucket('www.jackwkinsey.com')
-build_zip = io.BytesIO()
 
-# Download build.zip file
-print('Downloading build.zip...')
-build_bucket.download_fileobj('build.zip', build_zip)
-print('FINISHED!')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    build_bucket = s3.Bucket('codebuild.jackwkinsey.com')
+    portfolio_bucket = s3.Bucket('www.jackwkinsey.com')
+    build_zip = io.BytesIO()
 
-# Upload files from zip archive
-print('Uploading files from build.zip to portfolio bucket...')
-with zipfile.ZipFile(build_zip) as myzip:
-    for name in myzip.namelist():
-        print('Uploading ' + name)
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(
-            obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
-print('FINISHED!')
+    # Download build.zip file
+    print('Downloading build.zip...')
+    build_bucket.download_fileobj('build.zip', build_zip)
+    print('FINISHED!')
+
+    # Upload files from zip archive
+    print('Uploading files from build.zip to portfolio bucket...')
+    with zipfile.ZipFile(build_zip) as myzip:
+        for name in myzip.namelist():
+            print('Uploading ' + name)
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(
+                obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    print('FINISHED!')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File Upload Complete!')
+    }
