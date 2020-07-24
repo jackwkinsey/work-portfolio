e6564a9ccad6cd3613b3373c6d72df50e629503d
Jack Kinsey <jack@hexbitstudios.com>
2020-07-23 20:34:03 -0400

    feat: update lambda to use codebuild artifacts

--- upload-lambda.py
+++ upload-lambda.py
@@ -10,15 +10,27 @@ def lambda_handler(event, context):
     topic = sns.Topic(
         'arn:aws:sns:us-east-2:144055463112:deploy_portfolio_topic')
 
+    location = {
+        "bucketName": 'codebuild.jackwkinsey.com',
+        "objectKey": 'build.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+
+        print('Building portfolio from ' + str(location))
         s3 = boto3.resource('s3')
-        build_bucket = s3.Bucket('codebuild.jackwkinsey.com')
         portfolio_bucket = s3.Bucket('www.jackwkinsey.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         build_zip = io.BytesIO()
 
         # Download build.zip file
         print('Downloading build.zip...')
-        build_bucket.download_fileobj('build.zip', build_zip)
+        build_bucket.download_fileobj(location["objectKey"], build_zip)
         print('FINISHED!')
 
         # Upload files from zip archive
@@ -34,6 +46,11 @@ def lambda_handler(event, context):
 
         topic.publish(Subject='[SUCCESS] Portfolio Deployed',
                       Message='Portfolio deployed successfully!')
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
         return {
             'statusCode': 200,
             'body': json.dumps('Deployment complete!')
