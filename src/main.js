93f8e29f1e51645b0b92a66791ccd057520cc960
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-08-05 23:37:56 -0400

    feat: add dummy data for work examples

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import ExampleWork from './components/ExampleWork.js';
 const myWork = [
   {
     title: 'Portfolio',
+    href: 'https://www.example.com',
+    description:
+      'Nostrud minim minim ea consequat laborum commodo reprehenderit. Adipisicing aliquip ad magna labore veniam eiusmod quis proident exercitation. Velit consectetur nulla est velit aliquip eu in minim id elit quis reprehenderit consectetur. Cupidatat quis cupidatat non ullamco ad nisi fugiat enim labore deserunt anim ad adipisicing. Et labore nisi eiusmod consectetur anim incididunt veniam do. Cupidatat veniam officia dolore elit fugiat labore esse elit.',
     image: {
       description: 'example screenshot of a project involving code',
       src: 'images/example1.png',
@@ -13,6 +16,9 @@ const myWork = [
   },
   {
     title: 'Chemistry Project',
+    href: 'https://www.example.com',
+    description:
+      'Et adipisicing aliqua culpa sint nulla minim nostrud. Magna nulla dolor aliquip dolor proident enim culpa elit aute tempor dolor excepteur aliqua ea. Nostrud ea ut tempor consequat velit. Id incididunt minim sint sunt. Exercitation laboris non ipsum laboris minim. Adipisicing in laboris labore nisi esse est eiusmod veniam ea ullamco. Anim laboris Lorem minim do.',
     image: {
       description: 'example screenshot of a project involving chemistry',
       src: 'images/example2.png',
@@ -22,6 +28,9 @@ const myWork = [
   },
   {
     title: 'Cats Project',
+    href: 'https://www.example.com',
+    description:
+      'Nisi aliqua cupidatat adipisicing aliqua. Commodo sint occaecat nostrud ut fugiat eiusmod excepteur nostrud laborum sit. Veniam exercitation reprehenderit eu consequat est labore eiusmod Lorem velit proident. Quis eiusmod consequat dolore dolor pariatur velit consectetur ea laboris ipsum. Eiusmod cillum incididunt sint Lorem elit nostrud dolore officia do eu Lorem fugiat. Aute enim aliquip aliqua pariatur eiusmod adipisicing amet mollit velit labore voluptate.',
     image: {
       description: 'example screenshot of a project involving cats',
       src: 'images/example3.png',
