5c6d545cab0b464d40732faa31e82ee01096c191
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-08-16 21:57:11 -0400

    fix: use default mimetype if it can't be guessed
    This may need to be changed as it was failing to guess the mimetype on
    `*.map` files. I think it is fine for these to be `text/plain` type but
    I am not 100% sure.

--- upload-lambda.py
+++ upload-lambda.py
@@ -4,12 +4,10 @@ import io
 import zipfile
 import mimetypes
 
-
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
-    topic = sns.Topic(
-        'arn:aws:sns:us-east-2:144055463112:deploy_portfolio_topic')
-
+    topic = sns.Topic('arn:aws:sns:us-east-2:144055463112:deploy_portfolio_topic')
+    
     location = {
         "bucketName": 'codebuild.jackwkinsey.com',
         "objectKey": 'build.zip'
@@ -21,41 +19,40 @@ def lambda_handler(event, context):
             for artifact in job["data"]["inputArtifacts"]:
                 if artifact["name"] == "BuildArtifact":
                     location = artifact["location"]["s3Location"]
-
+        
         print('Building portfolio from ' + str(location))
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('www.jackwkinsey.com')
         build_bucket = s3.Bucket(location["bucketName"])
         build_zip = io.BytesIO()
-
+        
         # Download build.zip file
         print('Downloading build.zip...')
         build_bucket.download_fileobj(location["objectKey"], build_zip)
         print('FINISHED!')
-
+        
         # Upload files from zip archive
         print('Uploading files from build.zip to portfolio bucket...')
         with zipfile.ZipFile(build_zip) as myzip:
             for name in myzip.namelist():
                 print('Uploading ' + name)
+                mimetype = mimetypes.guess_type(name)[0] or 'text/plain'
                 obj = myzip.open(name)
                 portfolio_bucket.upload_fileobj(
-                    obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+                    obj, name, ExtraArgs={'ContentType': mimetype})
                 portfolio_bucket.Object(name).Acl().put(ACL='public-read')
         print('FINISHED!')
-
-        topic.publish(Subject='[SUCCESS] Portfolio Deployed',
-                      Message='Portfolio deployed successfully!')
-
+        
+        topic.publish(Subject='[SUCCESS] Portfolio Deployed', Message='Portfolio deployed successfully!')
+        
         if job:
             codepipeline = boto3.client('codepipeline')
             codepipeline.put_job_success_result(jobId=job["id"])
-
+        
         return {
             'statusCode': 200,
             'body': json.dumps('Deployment complete!')
         }
     except:
-        topic.publish(Subject='[FAIL] Portfolio Not Deployed',
-                      Message='Portfolio was not deployed.')
+        topic.publish(Subject='[FAIL] Portfolio Not Deployed', Message='Portfolio was not deployed.')
         raise
