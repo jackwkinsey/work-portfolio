7b2dae0c652ce6f2d8b30370d78e8ce096074b12
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-08-17 00:07:59 -0400

    test: add PageSection tests

---
+++ src/components/PageSection/tests/PageSection.spec.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+// How can I have path aliases so this line can be:
+// import { Component } from '@/components/PageSection'?
+import { PageSection } from '..';
+
+describe('PageSection component', () => {
+  describe('smoke test', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<PageSection />, div);
+    });
+  });
+
+  describe('template', () => {
+    it('has a section title', () => {
+      // Act
+      const wrapper = shallow(<PageSection />);
+
+      // Assert
+      expect(wrapper.exists('[data-testid="PageSection.sectionTitle"]')).toBe(true);
+    });
+
+    it('has a content image when image prop is provided', () => {
+      // Arrange
+      const image = {};
+
+      // Act
+      const wrapper = shallow(<PageSection image={image} />);
+
+      // Assert
+      expect(wrapper.exists('[data-testid="PageSection.contentImage"]')).toBe(true);
+    });
+
+    it('has content text', () => {
+      // Act
+      const wrapper = shallow(<PageSection />);
+
+      // Assert
+      expect(wrapper.exists('[data-testid="PageSection.contentText"]'));
+    });
+  });
+});
